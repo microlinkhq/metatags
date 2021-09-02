25509f50d2806d017d1a1a6ce4da340baf1574ee
Kiko Beats <josefrancisco.verdu@gmail.com>
2021-09-02 17:40:47 +0200

    chore: support schemaless urls

--- packages/cli/bin/cli/get-url.js
+++ packages/cli/bin/cli/get-url.js
@@ -1,4 +1,9 @@
 'use strict'
 
-module.exports = input =>
-  input || process.env.DEPLOY_URL || process.env.DEPLOY_PRIME_URL || process.env.URL
+const prependHttp = require('prepend-http')
+
+module.exports = input => {
+  const value = input || process.env.DEPLOY_URL || process.env.DEPLOY_PRIME_URL || process.env.URL
+  const collection = Array.isArray(value) ? value : value.split(',').map(item => item.trim())
+  return collection.map(prependHttp)
+}
